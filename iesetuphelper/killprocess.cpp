7d5ca03c617246375675f223f066f3bd1846811a
Ondřej Vondra <ondrej.vondra@live.com>
2012-05-02 15:23:58 +0200

    added KillAllProcessesByAboslutePath

---
+++ iesetuphelper/killprocess.cpp
@@ -0,0 +1,59 @@
+#include "stdafx.h"
+#include <tlhelp32.h>
+#include <Psapi.h>
+
+BOOL PathsEqual(const wchar_t *first, const wchar_t *second)
+{
+  HANDLE h1 = CreateFile(first, GENERIC_READ, 7 /* share all */, NULL, OPEN_EXISTING, 0, NULL);
+  if (h1 == INVALID_HANDLE_VALUE)
+  {
+    return FALSE;
+  }
+
+  HANDLE h2 = CreateFile(second, GENERIC_READ, 7 /* share all */, NULL, OPEN_EXISTING, 0, NULL);
+  if (h2 == INVALID_HANDLE_VALUE)
+  {
+    CloseHandle(h1);
+    return FALSE;
+  }
+
+  BOOL result = FALSE;
+  BY_HANDLE_FILE_INFORMATION fileInfo1, fileInfo2;
+  if (GetFileInformationByHandle(h1, &fileInfo1) && GetFileInformationByHandle(h2, &fileInfo2))
+  {
+    result = ((fileInfo1.dwVolumeSerialNumber == fileInfo2.dwVolumeSerialNumber) &&
+              (fileInfo1.nFileIndexHigh == fileInfo2.nFileIndexHigh) &&
+              (fileInfo1.nFileIndexLow == fileInfo2.nFileIndexLow));
+  }
+
+  CloseHandle(h2);
+  CloseHandle(h1);
+  return result;
+}
+
+EXPORTED(void, KillAllProcessesByAboslutePath)(const wchar_t *exeAbsolutePath)
+{
+  PROCESSENTRY32 entry;
+  entry.dwSize = sizeof(PROCESSENTRY32);
+  WCHAR exePath[MAX_PATH + 1];
+
+  HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+
+  if (Process32First(snapshot, &entry))
+  {
+    do {
+      HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, entry.th32ProcessID);
+      if (hProcess && (GetModuleFileNameEx(hProcess, NULL, exePath, MAX_PATH) > 0))
+      {
+        if (PathsEqual(exePath, exeAbsolutePath))
+        {
+          TerminateProcess(hProcess, 0);
+        }
+      }
+
+      CloseHandle(hProcess);
+    } while (Process32Next(snapshot, &entry));
+  }
+
+  CloseHandle(snapshot);
+}
